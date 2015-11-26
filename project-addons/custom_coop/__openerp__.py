b59b490b9d355b1b5df7decb157c1c02f0394894
jesusVMayor <jesus@comunitea.com>
2015-11-26 10:59:53 +0100

    [ADD]custom_coop: Añadido asistente para actualizar stock en ubicaciones.

---
+++ project-addons/custom_coop/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Comunitea All Rights Reserved
+#    $Jesús Ventosinos Mayor <jesus@comunitea.com>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Coop customizations',
+    'version': '1.0',
+    'category': '',
+    'description': """
+            -Update stock wizard.
+        """,
+    'author': 'Comunitea',
+    'website': '',
+    "depends": ['acc_analytic_acc_distribution_between', 'automatic_company', 'company_assign_users', 'company_automatic_account_config', 'company_open_fiscalyear', 'custom_colors', 'custom_email_template', 'custom_groups', 'custom_menu', 'protect_cud_parent_companies', 'res_partner_farm_data', 'simplify_invoice', 'supplier_type'],
+    "data": ['wizard/stock_location_update_stock.xml', 'views/stock.xml'],
+    "installable": True
+}
