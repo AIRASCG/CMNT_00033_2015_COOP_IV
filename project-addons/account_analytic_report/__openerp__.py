d626c9dd9e40deff5d5cd8d8447ebd1baf9c6688
jesusVMayor <jesus@comunitea.com>
2016-03-30 18:09:38 +0200

    [ADD]account_analytic_report: Añadido informe recría.

--- project-addons/account_analytic_report/__openerp__.py
+++ project-addons/account_analytic_report/__openerp__.py
@@ -30,6 +30,7 @@
     "data": ['views/account_analytic_report_template.xml', 'views/account_analytic_report.xml',
              'views/account_analytic_report_menu.xml', 'security/ir.model.access.csv',
              'views/report_account_analytic_report.xml', 'qweb_analytic_report.xml',
-             'data/pyg_1000.xml', 'data/pyg_cow.xml', 'data/pyg_employee.xml', 'data/pyg_ha.xml', 'data/technical_economic.xml'],
+             'data/pyg_1000.xml', 'data/pyg_cow.xml', 'data/pyg_employee.xml',
+             'data/pyg_ha.xml', 'data/technical_economic.xml', 'data/recria.xml'],
     "installable": True
 }
