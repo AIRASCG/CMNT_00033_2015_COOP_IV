e805015dbaa9cedba013d26de207bb3a0191ac50
jesusVMayor <jesus@comunitea.com>
2017-05-10 17:10:31 +0200

    [FIX]custom_project: Pequeñas modificaciones, ocultados campos, modificadas traducciones.

--- project-addons/custom_project/models/project.py
+++ project-addons/custom_project/models/project.py
@@ -10,6 +10,18 @@ class ProjectTaskWork(models.Model):
     _inherit = 'project.task.work'
 
     category_id = fields.Many2one('project.category', 'Tag')
+    task_id = fields.Many2one(string='Daily part')
     exploitation_id = fields.Many2one(
         'res.partner', 'Exploitation',
         domain="[('farm', '=', True),('is_cooperative','=',False)]")
+
+
+class ProjectTask(models.Model):
+
+    _inherit = 'project.task'
+
+    @api.model
+    def _get_default_name(self):
+        return fields.Date.from_string(fields.Date.today()).strftime('%Y%m%d')
+
+    name = fields.Char(default=_get_default_name)
