35d259d5d2204c04ae0eca7c23378a269ae9261c
jesusVMayor <jesus@comunitea.com>
2017-06-14 16:37:24 +0200

    [FIX]custom_project: Modificaciones en traducciones y fecha inicio/fin

--- project-addons/custom_project/models/project.py
+++ project-addons/custom_project/models/project.py
@@ -30,17 +30,14 @@ class ProjectTaskWork(models.Model):
 
     _inherit = 'project.task.work'
 
-    def _get_task_date(self):
-        return self._context.get('task_date', fields.Date.today())
-
     work_type = fields.Many2one('project.work.type')
     task_id = fields.Many2one(string='Daily part')
     user_id = fields.Many2one(compute='_compute_user_id')
     exploitation_id = fields.Many2one(
         'res.partner', 'Exploitation',
         domain="[('farm', '=', True),('is_cooperative','=',False)]")
-    date_start = fields.Datetime(default=_get_task_date)
-    date_end = fields.Datetime(default=_get_task_date)
+    date_start = fields.Float()
+    date_end = fields.Float()
     absence = fields.Boolean()
     absence_type = fields.Many2one('absence.type')
     area = fields.Many2one('project.category', related='task_id.area', readonly=True)
@@ -48,17 +45,12 @@ class ProjectTaskWork(models.Model):
     @api.onchange('date_start', 'date_end')
     def onchange_date_end(self):
         if self.date_start and self.date_end:
-            self.hours = (
-                datetime.strptime(self.date_end, '%Y-%m-%d %H:%M:%S') -
-                datetime.strptime(self.date_start, '%Y-%m-%d %H:%M:%S')
-                ).seconds / 60.0 / 60.0
+            self.hours = self.date_end - self.date_start
 
     @api.onchange('hours')
     def onchange_hours(self):
         if self.hours and self.date_start:
-            self.date_end = datetime.strptime(
-                self.date_start, '%Y-%m-%d %H:%M:%S') + \
-                    timedelta(hours=self.hours)
+            self.date_end = self.date_start + self.hours
 
     @api.depends('task_id.user_id')
     def _compute_user_id(self):
@@ -70,15 +62,13 @@ class ProjectTaskWork(models.Model):
         # Comprobamos que no se solape con otro project.task.work
         # del mismo usuario
         self.ensure_one()
-        concurrent_work = self.search(
-            ['&', '&', ('id', '!=', self.id),
-             ('user_id', '=', self.user_id.id), '|', '|', '&',
-             ('date_start', '>=', self.date_start),
-             ('date_start', '<=', self.date_end),
-             '&', ('date_end', '>=', self.date_start),
-             ('date_end', '<=', self.date_end),
-             '&', ('date_start', '<=', self.date_start),
-             ('date_end', '>=', self.date_start)])
+        concurrent_work = self.task_id.work_ids.filtered(
+            lambda r: (r.date_start > self.date_start and
+                       r.date_start < self.date_end) or
+                      (r.date_end > self.date_start and
+                       r.date_end < self.date_end) or
+                      (r.date_start < self.date_start and
+                       r.date_end > self.date_start))
         if concurrent_work:
             raise exceptions.Warning(
                 _('Work time error'),
